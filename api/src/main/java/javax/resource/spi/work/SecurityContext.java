a53c69b69d302a24196fd29c471c98298ed707b1
Rico Neubauer <r.neubauer@seeburger.de>
2015-01-20 17:05:05 +0100

    Replaced Unicode characters, that cause issues when building on some systems.

--- api/src/main/java/javax/resource/spi/work/SecurityContext.java
+++ api/src/main/java/javax/resource/spi/work/SecurityContext.java
@@ -30,33 +30,33 @@ import javax.security.auth.callback.CallbackHandler;
  * Work} instance to propagate security related context information from an EIS
  * to an application server.
  * <p>
- * 
- * 
+ *
+ *
  * This allows an EIS/resource adapter to flow-in security context information
  * and execute a Work instance, and call methods on a MessageEndpoint interface,
  * to effect message inflow, within that Work instance, in the context of an
  * established identity.
  * <p>
- * 
- * 
+ *
+ *
  * A resource adapter indicates to the WorkManager, that a Work instance needs
  * to be run in a specified security execution context by submitting a Work
  * instance that implements WorkContextProvider interface and ensuring that
  * the List of WorkContexts for that Work instance contains an instance of its
  * subclass of SecurityContext.
  * <p>
- * 
- * 
+ *
+ *
  * It should be noted however that when a resource adapter flows-in an identity
  * to be used by the application server, the propagated identity may or may not
  * belong to the application server's security domain.
  * <p>
- * 
- * 
+ *
+ *
  * There are therefore, two scenarios while a resource adapter propagates a
  * security identity from an EIS to the application server:
  * <p>
- * 
+ *
  * <ul>
  * <li>Case 1: Resource adapter flows-in an identity in the application server's
  * security domain: In this case, the application server could just set the
@@ -70,41 +70,41 @@ import javax.security.auth.callback.CallbackHandler;
  * performed.</li>
  * </ul>
  * <p>
- * 
+ *
  * @since 1.6
  * @see javax.resource.spi.work.WorkContextProvider
  * @version Java EE Connector Architecture 1.6
  */
 
-public abstract class SecurityContext implements WorkContext 
+public abstract class SecurityContext implements WorkContext
 {
    /**
     * Determines if a deserialized instance of this class
     * is compatible with this class.
     */
    private static final long serialVersionUID = 7730296651802712658L;
-   
+
    /**
     * {@inheritDoc}
     */
-   public String getDescription() 
+   public String getDescription()
    {
       return "Security Context";
    }
-   
+
    /**
     * {@inheritDoc}
     */
-   public String getName() 
+   public String getName()
    {
       return "SecurityContext";
    }
-   
+
    /**
     * The container calls this method to set up the security Context for the
     * <code>Work</code> instance.
     * <p>
-    * 
+    *
     * The handler argument must not be null, and the argument handler and the
     * <code>CallbackHandler</code> passed to this method must support the
     * following <code>Callback</code>s defined in JSR 196: Java Authentication
@@ -127,17 +127,17 @@ public abstract class SecurityContext implements WorkContext
     * <li>
     * </ul>
     * <p>
-    * 
-    * A resource adapter might use the <code>CallerPrincipalCallback</code> “to
+    *
+    * A resource adapter might use the <code>CallerPrincipalCallback</code> "to
     * set the container's representation of the caller principal. The
     * CallbackHandler must establish the argument Principal as the caller
     * principal associated with the invocation being processed by the
     * container. When the argument Principal is null, the handler will
     * establish the container's representation of the unauthenticated caller
-    * principal.”
+    * principal."
     * <p>
-    * 
-    * A resource adapter might use the <code>GroupPrincipalCallback</code> “to
+    *
+    * A resource adapter might use the <code>GroupPrincipalCallback</code> "to
     * establish the container's representation of the corresponding group
     * principals within the Subject. When a null value is passed to the groups
     * argument, the handler will establish the container's representation of no
@@ -145,19 +145,19 @@ public abstract class SecurityContext implements WorkContext
     * of this callback is additive, yielding the union (without duplicates) of
     * the principals existing within the Subject, and those created with the
     * names occuring within the argument array. The CallbackHandler will define
-    * the type of the created principals.”
+    * the type of the created principals."
     * <p>
-    * 
+    *
     * A resource adapter might use the <code>PasswordValidationCallback</code>
-    * “to employ the password validation facilities of its containing runtime.”
+    * "to employ the password validation facilities of its containing runtime."
     * <p>
-    * 
+    *
     * The executionSubject argument must be non-null and it must not be
     * read-only. It is expected that this method will populate this
     * executionSubject with principals and credentials that would be flown into
     * the application server.
     * <p>
-    * 
+    *
     * The serviceSubject argument must be non-null and it must not be
     * read-only. It represents the application server and it may be used by the
     * Work implementation to retrieve Principals and credentials necessary to
@@ -166,8 +166,8 @@ public abstract class SecurityContext implements WorkContext
     * collect the server credentials, as necessary, by using the callback
     * handler passed to them .
     * <p>
-    * 
-    * 
+    *
+    *
     * When this method is called, the method implementation
     * <ul>
     * <li>identifies the security context that needs to be flown-in to the
@@ -186,10 +186,10 @@ public abstract class SecurityContext implements WorkContext
     * execution context of the Work instance</li>
     * </ul>
     * <p>
-    * 
+    *
     * @see JSR 196: Java Authentication SPI for Containers specification and
     *      related JavaDoc
-    * 
+    *
     * @param handler
     *            A <code>CallbackHandler</code> provided by the
     *            <code>WorkManager</code> that supports the
